35e0ac3b9b5306d12ab1516c23b69c37ee6f5009
kallistahalim <kallistahalim@gmail.com>
2020-02-17 16:11:00 -0800

    add input form

---
+++ my-app/src/component/input/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Buttons from '../buttons/index';
+
+export default function index() {
+    return (
+            <form>
+            <input type="text" id="value" name="calculatorvalue">
+            </form>
+        
+    )
+   
+}
