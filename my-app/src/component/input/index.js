90b2b30b00b6877ccc240e6aabf42c4b10fd613d
kallistahalim <kallistahalim@gmail.com>
2020-02-19 13:54:35 -0800

    color it a little bit

--- my-app/src/component/input/index.js
+++ my-app/src/component/input/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Buttons from '../buttons/index';
+import '../input/index.css';
 
 export default function index() {
     return (
--- my-app/src/component/title/index.js
+++ my-app/src/component/title/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Buttons from '../buttons/index';
+import Input from '../input/index';
+import "./index.css";
 
 export default function index() {
     return (
         <div>
-        <p>Calculator</p>
+        <p className="title">Calculator</p>
+        <Input />
         <Buttons />
         </div> 
     )
